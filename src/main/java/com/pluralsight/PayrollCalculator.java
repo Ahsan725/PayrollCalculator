e9b302dddf993ab5f9654a87f7306085821a2aa0
Ahsan <sbaseer25@gmail.com>
2025-10-07 15:05:16 -0400

    done!

---
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class PayrollCalculator {
+
+    public static void main(String[] args) {
+
+        String filename = "EmployeeData.txt";
+
+        // Use try-with-resources.
+        // FileNotFoundException might be thrown here by the FileReader constructor.
+        try(BufferedReader br =  new BufferedReader(new FileReader(filename))){
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                var tokens = line.split("\\|");
+                int empId = Integer.parseInt(tokens[0]);
+                double hours = Double.parseDouble(tokens[2]);
+                double payRate = Double.parseDouble(tokens[3]);
+                Employee emp = new Employee(empId,tokens[1],hours,payRate);
+                System.out.println("Employee Name: " + emp.getName() + ", Employee ID: " + emp.getEmployeeId() + ", Gross Pay: " + emp.getGrossPay());
+
+            }
+        } catch (IOException e) {
+            System.out.println("Problem reading the file: " + e.getMessage());
+             e.printStackTrace();
+        }
+    }
+}
